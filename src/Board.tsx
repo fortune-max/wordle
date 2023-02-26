1d9e7dd1877e62e163879200dfb0e539d8c3310c
fortune-max <fortmax120@gmail.com>
2023-02-26 19:08:06 +0100

    add Board component

---
+++ src/Board.tsx
@@ -0,0 +1,31 @@
+import Row from './Row';
+import React from 'react';
+import styled from 'styled-components';
+
+const BoardElement = styled.div<{}>`
+    display: flex;
+    flex-direction: column;
+    gap: 3px;
+`;
+
+function Board({ rowCount, guesses, correctWord, currentGuess, wordLength } : {
+    rowCount: number;
+    guesses: string[];
+    correctWord: string;
+    currentGuess: string;
+    wordLength: number;
+}) {
+    return (
+        <BoardElement>
+            {guesses.map((guess, index) => {
+                return <Row key={index} currentWord={guess} correctWord={correctWord} wordLength={wordLength} />;
+            })}
+            <Row key={guesses.length} currentWord={currentGuess} correctWord={correctWord} wordLength={wordLength} />
+            {Array(rowCount - guesses.length).fill(0).map((_, index) => {
+                return <Row key={guesses.length + index + 1} currentWord={""} correctWord={correctWord} wordLength={wordLength} />;
+            })}
+        </BoardElement>
+    );
+}
+
+export default Board;
